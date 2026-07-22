5ed75ae43f7f0b5e92dceb46d211d06f97c52dad
jia-66666 <kejiaxiang.666@gmail.com>
2026-07-22 23:20:20 +0800

    navbar created & linked - jx

---
+++ assets/components/navbar.js
@@ -0,0 +1,33 @@
+class Navbar extends HTMLElement {
+    connectedCallback() {
+        const currentPath = window.location.pathname;
+        this.innerHTML = `
+<nav class="navbar navbar-expand-lg bg-primary" aria-label="Main Navigation">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+`;
+    }
+}
+
+window.customElements.define('nav-bar', Navbar)
